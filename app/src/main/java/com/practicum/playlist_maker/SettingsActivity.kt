b20edd9caabc5832d740d8297c0ad734e8f0ddb0
Vladislav <bigmag174@yandex.ru>
2023-02-10 13:44:08 +0500

    first lesson changes

---
+++ app/src/main/java/com/practicum/playlist_maker/SettingsActivity.kt
@@ -0,0 +1,16 @@
+package com.practicum.playlist_maker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+        val backButton = findViewById<ImageView>(R.id.back_btn)
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
