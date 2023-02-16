561c353264a1f3370e16210a88077899c7858b74
Vladislav <bigmag174@yandex.ru>
2023-02-16 13:58:16 +0500

    All tasks of Sprint 9

--- app/src/main/java/com/practicum/playlist_maker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlist_maker/SearchActivity.kt
@@ -2,10 +2,68 @@ package com.practicum.playlist_maker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+    }
+
+    lateinit var searchEditText: EditText
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchEditText.text.toString())
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchEditText.setText(savedInstanceState.getString(SEARCH_TEXT, ""))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        searchEditText = findViewById<EditText>(R.id.searchEditText)
+        val crossButton = findViewById<ImageView>(R.id.crossButton)
+
+        val searchButtonTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                crossButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        }
+
+        searchEditText.addTextChangedListener(searchButtonTextWatcher)
+
+        crossButton.setOnClickListener {
+            searchEditText.setText("")
+        }
+
+    }
+
+}
+
+private fun clearButtonVisibility(s: CharSequence?): Int {
+    return if (s.isNullOrEmpty()) {
+        View.GONE
+    } else {
+        View.VISIBLE
     }
 }
